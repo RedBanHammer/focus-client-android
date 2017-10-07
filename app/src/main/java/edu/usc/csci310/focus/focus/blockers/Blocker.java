763ed3b4d00cc5bf891188f08ccb2f66d0eb259a
RedBanHammer <redbanhammer@gmail.com>
2017-10-07 15:21:24 -0700

    Blockers: Add Blocker interface

--- app/src/main/java/edu/usc/csci310/focus/focus/blockers/Blocker.java
+++ app/src/main/java/edu/usc/csci310/focus/focus/blockers/Blocker.java
@@ -1,9 +1,27 @@
 package edu.usc.csci310.focus.focus.blockers;
 
+import java.util.ArrayList;
+
+import edu.usc.csci310.focus.focus.dataobjects.App;
+
 /**
  * Base blocker interface that Blockers should conform to.
  */
 
 abstract interface Blocker {
+    /**
+     * Set the list of apps to block by this blocker module.
+     * @param apps An ArrayList of apps that should be blocked.
+     */
+    public void setApps(ArrayList<App> apps);
 
+    /**
+     * Start blocking apps.
+     */
+    public void startBlocking();
+
+    /**
+     * Stop blocking apps.
+     */
+    public void stopBlocking();
 }
