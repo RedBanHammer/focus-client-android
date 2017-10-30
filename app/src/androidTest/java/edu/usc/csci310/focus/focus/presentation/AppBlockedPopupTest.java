5a9641e5ed18ac7193db38248d7f8d3b53700c44
Ashley Walker <ashleylw@usc.edu>
2017-10-29 20:02:15 -0700

    Test: AppBlockedPopupTest successfully tests whether the popup launches

--- app/src/androidTest/java/edu/usc/csci310/focus/focus/presentation/AppBlockedPopupTest.java
+++ app/src/androidTest/java/edu/usc/csci310/focus/focus/presentation/AppBlockedPopupTest.java
@@ -1,156 +1,51 @@
 package edu.usc.csci310.focus.focus.presentation;
 
-import android.content.Context;
-import android.os.Bundle;
-import android.support.test.InstrumentationRegistry;
-import android.test.mock.MockContext;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
-import java.io.File;
-import java.util.Calendar;
-import java.util.HashMap;
-
-import edu.usc.csci310.focus.focus.R;
+import edu.usc.csci310.focus.focus.MainActivity;
 import edu.usc.csci310.focus.focus.dataobjects.App;
-import edu.usc.csci310.focus.focus.dataobjects.Profile;
-import edu.usc.csci310.focus.focus.dataobjects.RecurringTime;
-import edu.usc.csci310.focus.focus.dataobjects.Schedule;
-import edu.usc.csci310.focus.focus.dataobjects.Timer;
-import edu.usc.csci310.focus.focus.managers.ProfileManager;
-import edu.usc.csci310.focus.focus.managers.ScheduleManager;
-import edu.usc.csci310.focus.focus.storage.StorageManager;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
 /**
  * Created by Ashley Walker on 10/28/2017.
  */
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(AndroidJUnit4.class)
 public class AppBlockedPopupTest {
-
-    Context context;
-
     @Rule
-    public TemporaryFolder tmpFileDir = new TemporaryFolder();
+    public IntentsTestRule<MainActivity> mActivityRule;
 
-    @Mock
-    private ProfileManager mockedProfileManager;
-    private ScheduleManager mockedScheduleManager;
-
-    private AppBlockedPopup testedABP;
-
-    private Profile testProfile1;
-    private HashMap<String, Profile> profileHashMap;
-
-    private Schedule testSchedule1;
-    // Maps schedule name -> Schedule
-    private HashMap<String, Schedule> scheduleHashMap;
-
-    // Test Apps
+    // Test App
     private final App testApp1 = new App("YouTube", "com.google.android.youtube");
-    private final App testApp2 = new App("Gmail", "com.google.android.gmail");
-    private final App testApp3 = new App("Messages", "com.google.android.messages");
 
     @Before
     public void setUp() {
-        this.setUpManagers();
-        this.setUpProfiles();
-        this.setUpSchedule();
-    }
+        Intent intent = new Intent(mActivityRule.getActivity(), SplashScreen.class);
 
-    private void setUpProfiles() {
-        this.testProfile1 = new Profile("Test Profile 1");
-        this.testProfile1.addApp(this.testApp1);
-        this.testProfile1.addApp(this.testApp2);
-        this.testProfile1.addApp(this.testApp3);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // You need this if starting
+        //  the activity from a service
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.putExtra("appName", this.testApp1.getName());
 
-        this.mockedProfileManager.setProfile(this.testProfile1);
-
-        this.profileHashMap = new HashMap<>();
-        this.profileHashMap.put(this.testProfile1.getIdentifier(), this.testProfile1);
-    }
-
-    private void setUpSchedule() {
-        this.testSchedule1 = new Schedule("Test Schedule 1");
-        RecurringTime rt = new RecurringTime();
-        Calendar c = Calendar.getInstance();
-        rt.addTime(c.get(Calendar.DAY_OF_WEEK)-1,
-                (long)(c.get(Calendar.HOUR)*60 + c.get(Calendar.MINUTE)),
-                (long) 10);
-        this.testSchedule1.addProfile(testProfile1.getIdentifier(), rt);
-
-        this.mockedScheduleManager.setSchedule(this.testSchedule1);
-
-        this.scheduleHashMap = new HashMap<>();
-        this.scheduleHashMap.put(this.testSchedule1.getIdentifier(), this.testSchedule1);
-    }
-
-    private void setUpManagers() {
-        this.mockedProfileManager = mock(ProfileManager.class);
-        this.mockedScheduleManager = mock(ScheduleManager.class);
-
-        this.scheduleHashMap = new HashMap<>();
-        when(this.mockedScheduleManager.getScheduleWithName(anyString())).thenAnswer(
-                new Answer<Schedule>() {
-                    @Override
-                    public Schedule answer(InvocationOnMock invocation) {
-                        Object[] args = invocation.getArguments();
-                        String name = (String)args[0];
-                        return scheduleHashMap.get(name);
-                    }});
-        doAnswer(new Answer<Object>() {
-            @Override
-            public Object answer(InvocationOnMock invocation) {
-                Object[] args = invocation.getArguments();
-                Schedule schedule = (Schedule)args[0];
-
-                scheduleHashMap.put(schedule.getName(), schedule);
-
-                return null;
-            }}).when(this.mockedScheduleManager).setSchedule(any(Schedule.class));
-
-        when(this.mockedProfileManager.getProfileWithIdentifier(anyString())).thenAnswer(
-                new Answer<Profile>() {
-                    @Override
-                    public Profile answer(InvocationOnMock invocation) {
-                        Object[] args = invocation.getArguments();
-                        String identifier = (String)args[0];
-                        return profileHashMap.get(identifier);
-                    }});
+        mActivityRule.launchActivity(intent);
     }
 
     @Test
-    public void onCreateDialog() throws Exception {
-        context = InstrumentationRegistry.getInstrumentation().getContext();
-        StorageManager testedManager = StorageManager.getDefaultManagerWithContext(context);
-        File baseDir = this.tmpFileDir.newFolder("app-blocked-popup-test");
-
-        Bundle savedInstanceState = new Bundle();
-        savedInstanceState.putString("profile", "profileName");
-        boolean containsKey = savedInstanceState.containsKey("profile");
-        assertTrue(containsKey);
-
-        testedABP = new AppBlockedPopup();
-        testedABP.onCreateDialog(savedInstanceState);
-
-        String message = testedABP.getDialog().toString();
-        String expected = "appName is being blocked by the following schedule:\n\n• scheduleName\n";
-        assertEquals(expected, message);
+    public void testPopup() throws Exception {
+        // Check that the AppBlockedPopup is indeed up.
+        intended(hasComponent(new ComponentName(getTargetContext(), AppBlockedPopup.class)));
     }
 
 }
