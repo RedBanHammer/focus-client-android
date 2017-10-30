229901d579d9bf8ed0bd51210ab1223aab7e3ce4
RedBanHammer <redbanhammer@gmail.com>
2017-10-29 20:35:11 -0700

    Schedule List Test: Add delay to compensate for fragment animations
    Fixes bug where Schedule list adapter was attempting to filter out
    timers and crashing on schedules without any added profiles.

--- app/src/androidTest/java/edu/usc/csci310/focus/focus/presentation/schedule/ScheduleInterfaceControllerTest.java
+++ app/src/androidTest/java/edu/usc/csci310/focus/focus/presentation/schedule/ScheduleInterfaceControllerTest.java
@@ -1,10 +1,14 @@
 package edu.usc.csci310.focus.focus.presentation.schedule;
 
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,6 +25,7 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.isDialog;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
@@ -30,6 +35,31 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ScheduleInterfaceControllerTest {
+
+    /**
+     * Perform action of waiting for a specific time.
+     * @source https://stackoverflow.com/a/35924943
+     */
+    public static ViewAction waitFor(final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Wait for " + millis + " milliseconds.";
+            }
+
+            @Override
+            public void perform(UiController uiController, final View view) {
+                uiController.loopMainThreadForAtLeast(millis);
+            }
+        };
+    }
+
+
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
@@ -37,6 +67,9 @@ public class ScheduleInterfaceControllerTest {
     public void init(){
         onView(withId(R.id.viewPager)).check(matches(isDisplayed()));
         onView(withId(R.id.viewPager)).perform(swipeLeft());
+
+        onView(isRoot()).perform(waitFor(1000));
+
         ViewPager viewPager = (ViewPager) activityTestRule.getActivity().findViewById(R.id.viewPager);
         ViewPagerIdlingResource idling = new ViewPagerIdlingResource(viewPager, "View Pager");
     }
@@ -46,6 +79,9 @@ public class ScheduleInterfaceControllerTest {
         onView(withText("OK")).check(matches(isDisplayed()));
         onView(withId(R.id.create_schedule_name)).check(matches(isDisplayed()));
         onView(withId(R.id.create_schedule_name)).perform(typeText("Schedule 1"));
+
+        onView(isRoot()).perform(waitFor(500));
+        
         onView(withId(android.R.id.button1)).perform(click());
     }
 
