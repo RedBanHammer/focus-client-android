c79e438e16f42ef254deffc98fe65185b59871bf
Ashley Walker <ashleylw@usc.edu>
2017-11-04 13:42:53 -0700

    Popup: fixed double popup when app is blocked

--- app/src/main/java/edu/usc/csci310/focus/focus/blockers/AppBlocker.java
+++ app/src/main/java/edu/usc/csci310/focus/focus/blockers/AppBlocker.java
@@ -86,7 +86,7 @@ public class AppBlocker extends IntentService implements Blocker {
                             break;
                         }
                     }
-                    
+
 //                }
 //            }
 
